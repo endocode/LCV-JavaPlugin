45d748e72909122b6276327e78644de7d0175d67
Michele Scarlato <michele.scarlato@gmail.com>
2021-04-13 12:24:06 +0900

    Added json-java dependency. Errors retrieving a particular field of the json (InboundLicenses)

--- PostgreSqlConnector.java
+++ PostgreSqlConnector.java
@@ -4,6 +4,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONString;
 
 
 public class PostgreSqlConnector {
@@ -15,6 +19,8 @@ public class PostgreSqlConnector {
 		String host = properties.getPropValues("host");
 		String port = properties.getPropValues("port");
 		String db = properties.getPropValues("db");
+		JSONObject json = new JSONObject();
+
 
 
 		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://"+host+":"+port+"/"+db+"", user, password)) {
@@ -23,11 +29,19 @@ public class PostgreSqlConnector {
 
 			System.out.println("Connected to PostgreSQL database!");
 			Statement statement = connection.createStatement();
-			System.out.println("Reading car records...");
-			System.out.printf("%-30.30s  %-30.30s%n", "id", "metadata");
+			System.out.println("Reading metadata records...");
+			//System.out.printf("%-30.30s  %-30.30s%n", "id", "metadata");
 			ResultSet resultSet = statement.executeQuery("SELECT * FROM package_version");
 			while (resultSet.next()) {
-				System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("id"), resultSet.getString("metadata"));
+				//System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("id"), resultSet.getString("metadata"));
+				String id = resultSet.getString("id");
+
+				Object metadata = resultSet.getObject("metadata");
+				//Object metadata = JSON.parse(resultSet.getString("metadata"));
+				System.out.println("id:"+id);
+				System.out.println("JSON Payload:"+metadata);
+				String InboundLicenses = json.getJSONObject("input").getString("InboundLicenses");
+
 			}
 		} catch (SQLException e) {
 			System.out.println("Connection failure.");
